ef434d7c6d7af74d71d5146ea15c89057bc28d91
James <jkeezer@pm.me>
2023-05-23 00:29:36 -0400

    Add cart type select

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
+import FormLabel from '@mui/joy/FormLabel';
+import Select from "@mui/material/Select";
+import MenuItem from '@mui/material/MenuItem';
 
 pokemon.configure({ apiKey: import.meta.env.API_KEY });
 
 export default function App() {
   const [params, setParams] = useState("");
+  const [subtype, setSubtype] = useState("*");
   const [searchResults, setSearchResults] = useState({ data: {} });
   const [searchType, setSearchType] = useState("name");
   const [warning, setWarning] = useState(false);
@@ -27,20 +31,31 @@ export default function App() {
     setSearchType(event.target.value);
   };
 
+  const handleSubtypeChange = (event) => {
+    setSubtype(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     setIsLoading(true);
     event.preventDefault();
-    console.log(isLoading);
     if (params.length < 3) {
       setWarning(true);
       setIsLoading(false);
       setSearchResults({ data: {} });
     } else {
       setWarning(false);
-      pokemon.card.where({ q: `${searchType}:*${params}*` }).then((result) => {
-        setSearchResults(result);
-        setIsLoading(false);
-      });
+      pokemon.card
+        .where({ q: `${searchType}:*${params}* subtypes:${subtype}` })
+        .then((result) => {
+          setSearchResults(result);
+          setIsLoading(false);
+        });
+    }
+  };
+
+  const keyPress = (e) => {
+    if (e.keyCode == 13) {
+      handleSubmit(e);
     }
   };
 
@@ -63,9 +78,11 @@ export default function App() {
         <FormControl className="w-9/12 sm:w-72">
           <TextField
             required
+            autoFocus
             id="outlined-basic"
-            label="Search Pokemon"
+            label="Search for Pokemon Cards"
             variant="outlined"
+            onKeyDown={keyPress}
             onChange={handleChange}
           />
           <RadioGroup
@@ -88,6 +105,20 @@ export default function App() {
               sx={{ margin: "0 auto", marginLeft: "1rem" }}
             />
           </RadioGroup>
+          <FormLabel sx={{ marginTop: "1rem", marginLeft: "1rem", fontSize: "1rem" }}>Card type</FormLabel>
+          <Select
+            value={subtype}
+            sx={{ margin: "1rem" }}
+            onChange={handleSubtypeChange}
+          >
+            <MenuItem value="*">All</MenuItem>
+            <MenuItem value="Basic">Basic</MenuItem>
+            <MenuItem value="EX">EX</MenuItem>
+            <MenuItem value="GX">GX</MenuItem>
+            <MenuItem value="MEGA">MEGA</MenuItem>
+            <MenuItem value="V">V</MenuItem>
+            <MenuItem value="VMAX">VMAX</MenuItem>
+          </Select>
           {isLoading ? (
             <Button
               loading
@@ -114,11 +145,15 @@ export default function App() {
           </Alert>
         )}
         {searchResults.data[0] && (
-          <p className="font-semibold">
+          <p className="font-semibold m-4">
             {searchResults.data.length + " results"}
           </p>
         )}
-        {searchResults.count === 0 && <p>Nothing found</p>}
+        {searchResults.count === 0 && (
+          <p className="m-4">
+            <strong>Nothing found</strong>
+          </p>
+        )}
         <div className="flex flex-row flex-wrap items-center justify-center">
           {searchResults.count !== 0 && <Cards />}
         </div>
